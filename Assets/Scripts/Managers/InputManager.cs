d1167a50f1996f6cdda7258425c9d7cbd8b975c8
LeeJaeUng <lsam3333333@gmail.com>
2024-11-20 16:38:12 +0900

    InputManager에서 액션 사용
    PlayerInput컴포넌트를 InputManager하나만 보유하고 액션을 사용해 움직임이 필요한 스크립트에서 액션을 등록해 사용

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,10 +6,30 @@ public class InputManager : Singleton<InputManager>
 {
     private PlayerInput playerInput;
 
+    public static event Action<Vector2> OnMoveAction;
+    public static event Action OnClick;
+
     protected override void Awake()
     {
-        base.Awake();  // 부모 클래스의 Awake() 호출
+        base.Awake();  
         playerInput = GetComponent<PlayerInput>();
+
+    }
+    private void OnEnable()
+    {
+        SwitchToPlayerControls();
+
+    }
+
+    private void HandleMove(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    void OnMove(InputValue inputValue)
+    {
+        print($"이동벡터 X값 : {inputValue.Get<Vector2>().x}, Y값 : {inputValue.Get<Vector2>().y}");
+        OnMoveAction?.Invoke(inputValue.Get<Vector2>());
     }
 
     public void SwitchToPlayerControls()
